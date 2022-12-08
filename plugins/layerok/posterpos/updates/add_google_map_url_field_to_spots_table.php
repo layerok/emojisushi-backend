df9edbbe3967a1ce267deb2cb4991ec8a44616e0
layerok <kotopes231@gmail.com>
2022-12-08 22:51:01 +0200

    add fields to spots table

---
+++ plugins/layerok/posterpos/updates/add_google_map_url_field_to_spots_table.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Layerok\PosterPos\Updates;
+
+use October\Rain\Database\Schema\Blueprint;
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * some_upgrade_file.php
+ */
+class AddGoogleMapUrlFieldToSpotsTable extends Migration
+{
+    ///
+    public function up()
+    {
+        Schema::table('layerok_posterpos_spots', function (Blueprint $table) {
+            $table->string('google_map_url')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('layerok_posterpos_spots', function (Blueprint $table) {
+            $table->dropColumn(['google_map_url']);
+        });
+    }
+}
+
+
