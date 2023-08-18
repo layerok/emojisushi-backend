0d38e3768724181de99853984a15bedeec6723b8
layerok <18424848+adjustmentlayer@users.noreply.github.com>
2023-08-18 07:01:03 +0300

    don't show unpublished spots

--- plugins/layerok/restapi/http/controllers/CityController.php
+++ plugins/layerok/restapi/http/controllers/CityController.php
@@ -17,19 +17,25 @@ class CityController extends Controller
 
         $query = City::query();
 
-        if($includeSpots) {
-            $query->with(['spots.photos', 'districts']);
+        if ($includeSpots) {
+            $query->with(['spots' => function ($q) {
+                $q->where('published', 1);
+            }]);
         }
 
-        if($includeDistricts) {
-            $query->with(['districts.spots']);
+        if ($includeDistricts) {
+            $query->with(['districts' => function ($q) {
+                $q->with(['spots' => function ($q) {
+                    $q->where('published', 1);
+                }]);
+            }]);
         }
 
-        if($limit) {
+        if ($limit) {
             $query->limit($limit);
         }
 
-        if($offset) {
+        if ($offset) {
             $query->offset($offset);
         }
 
@@ -45,21 +51,38 @@ class CityController extends Controller
         ]);
     }
 
-    public function one(): JsonResponse {
+    public function one(): JsonResponse
+    {
         $slug_or_id = input('slug_or_id');
 
-        $record = City::findBySlugOrId($slug_or_id);
+        $key = is_numeric($slug_or_id) ? 'id': 'slug';
+        $record = City::with(['spots' => function ($q) {
+            $q->where('published', 1);
+        }, 'districts' => function ($q) {
+            $q->with(['spots' => function ($q) {
+                $q->where('published', 1);
+            }]);
+        }])->where($key, $slug_or_id)->first();
 
-        if(!$record) {
-           return response()->json(['error' => 'Not Found!'], 404);
+        if (!$record) {
+            return response()->json(['error' => 'Not Found!'], 404);
         }
 
         return response()->json($record);
     }
 
-    public function main(): JsonResponse {
-        $city = City::with(['spots', 'districts.spots'])->where('is_main', 1)->first();
-        if($city) {
+    public function main(): JsonResponse
+    {
+        $city = City::with(['spots' => function ($q) {
+            $q->where('published', 1);
+        }, 'districts' => function ($q) {
+            $q->with(['spots' => function ($q) {
+                $q->where('published', 1);
+            }]);
+        }])
+            ->where('is_main', 1)
+            ->first();
+        if ($city) {
             return response()->json($city);
         }
         return response()->json(['error' => 'Not Found!'], 404);
