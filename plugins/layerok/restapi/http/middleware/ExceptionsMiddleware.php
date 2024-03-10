09c49b1fbacb8da6d3295360eff997137f8d07af
Vladimir <kotopes231@gmail.com>
2024-03-10 07:31:39 +0200

    convert AuthException from 500 code to 422

--- plugins/layerok/restapi/http/middleware/ExceptionsMiddleware.php
+++ plugins/layerok/restapi/http/middleware/ExceptionsMiddleware.php
@@ -6,6 +6,7 @@ use App;
 use Closure;
 use Illuminate\Http\Request;
 use Exception;
+use October\Rain\Auth\AuthException;
 use October\Rain\Exception\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -35,6 +36,9 @@ class ExceptionsMiddleware
             } else if ($exception instanceof  \Illuminate\Validation\ValidationException) {
                 $response['errors'] = $exception->errors();
                 $code = $exception->status;
+            } else if ($exception instanceof AuthException) {
+                $code = 422;
+                // todo: don't show to the user the detailed error
             }
 
             if($exception instanceof HttpException) {
