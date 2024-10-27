88d732dc0cafc89eb153b48f0dfdfd95af8773e7
Vladimir <kotopes231@gmail.com>
2024-10-27 23:02:52 +0200

    remove 'migrate' route

--- plugins/layerok/posterpos/routes.php
+++ plugins/layerok/posterpos/routes.php
@@ -17,35 +17,3 @@ Route::get('test', function () {
     return explode('.', request()->header('host'))[0];
 });
 
-Route::get('migrate', function () {
-    $kador = PosterAccount::where('account_name', 'emojisushikador')->first();
-    $bazarna = PosterAccount::where('account_name', 'emoji-bar2')->first();
-
-    // migrate models
-    $records = collect([
-        Property::all(),
-        PropertyGroup::all(),
-        Variant::all(),
-        Category::all(),
-        Product::all()
-    ])->flatten(2);
-
-    $records->each(function($record) use($bazarna, $kador) {
-        $relations = [];
-
-        if(!empty($record->poster_id) && !empty($bazarna)) {
-            $relations[$bazarna->id] = [
-                'poster_id' => $record->poster_id,
-            ];
-        }
-
-        if(!empty($record->poster_id2) && !empty($kador)) {
-            $relations[$kador->id] = [
-                'poster_id' => $record->poster_id2,
-            ];
-        }
-
-        $record->poster_accounts()->sync($relations);
-    });
-    return '';
-});
